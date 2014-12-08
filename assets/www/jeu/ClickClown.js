99da472d1d0a8f83ca05fc26a8e766ada92a8e91
geoffrey.glangine <geoffrey.glangine@638349cc-ab1b-4701-bd74-1ecb64fdc45a>
2014-12-08 19:43:44 +0000

    redimentionnement du canevas... Enfin !

--- assets/www/jeu/ClickClown.js
+++ assets/www/jeu/ClickClown.js
@@ -1,12 +1,20 @@
 var ClickClown = function(){
     this.jouer = function(){
-        var stage = new createjs.Stage("demoCanvas");
+        function resize_canvas(){
+            content = document.getElementById("content");
+            canvas = document.getElementById("gameCanvas");
+            canvas.width  = content.offsetWidth;
+            canvas.height = content.offsetHeight;
+
+        }
+        resize_canvas();
+        var stage = new createjs.Stage("gameCanvas");
         var circle = new createjs.Shape();
         circle.graphics.beginFill("red").drawCircle(0, 0, 50);
         circle.x = 100;
         circle.y = 100;
         stage.addChild(circle);
-        stage.addChild(new createjs.Shape()).setTransform(100,100).graphics.f("red").dc(0,0,50);
+        //stage.addChild(new createjs.Shape()).setTransform(100,100).graphics.f("red").dc(0,0,50);
         stage.update();
     }
 };
