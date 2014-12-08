c06e272b3226c1b6b5f1d3f60ad48f6227275906
geoffrey.glangine <geoffrey.glangine@638349cc-ab1b-4701-bd74-1ecb64fdc45a>
2014-12-08 20:06:16 +0000

    Déplacement d'un objet dans le stage !

--- assets/www/jeu/ClickClown.js
+++ assets/www/jeu/ClickClown.js
@@ -1,14 +1,17 @@
 var ClickClown = function(){
+    var stage = new createjs.Stage("gameCanvas");
     this.jouer = function(){
         function resize_canvas(){
             content = document.getElementById("content");
             canvas = document.getElementById("gameCanvas");
             canvas.width  = content.offsetWidth;
             canvas.height = content.offsetHeight;
+            stage.width = canvas.width;
+            stage.height = canvas.height;
 
         }
         resize_canvas();
-        var stage = new createjs.Stage("gameCanvas");
+
         var circle = new createjs.Shape();
         circle.graphics.beginFill("red").drawCircle(0, 0, 50);
         circle.x = 100;
@@ -16,5 +19,21 @@ var ClickClown = function(){
         stage.addChild(circle);
         //stage.addChild(new createjs.Shape()).setTransform(100,100).graphics.f("red").dc(0,0,50);
         stage.update();
+        createjs.Ticker.addEventListener("tick", enterFrameHandler);
+        createjs.Ticker.setInterval(25);
+        createjs.Ticker.setFPS(60);
+        var vitessex = 3;
+        var vitessey= 3;
+
+        function enterFrameHandler(event) {
+            if(((circle.x +50) >= stage.width) || (circle.x - 50) <= 0)
+                vitessex = -vitessex;
+            if(((circle.y +50) >= stage.height) || (circle.y - 50) <= 0)
+                vitessey = -vitessey;
+            circle.x += vitessex;
+            circle.y += vitessey;
+            stage.update();
+            // this will log a steadily increasing value:
+        }
     }
 };
