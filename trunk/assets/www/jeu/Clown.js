74d206478c2ecf03bff4d9c00280a19c9ab47fa8
geoffrey.glangine <geoffrey.glangine@638349cc-ab1b-4701-bd74-1ecb64fdc45a>
2014-12-19 22:59:58 +0000

    on repart sur de bonnes bases

---
+++ trunk/assets/www/jeu/Clown.js
@@ -0,0 +1,43 @@
+SpriteClown = function(couleur, supprimer, perdre){
+    this.supprimer = supprimer;
+    this.couleur = couleur;
+    this.data = {
+        images: ["img/SpriteSheetClown.png"],
+        frames: {width:256, height:256},
+        animations: {normal:[0],noir:[1],malade:[2]}
+    };
+
+    this.spriteSheet = new createjs.SpriteSheet(this.data);
+
+    if(this.couleur == "noir")
+        this.animation = new createjs.Sprite(this.spriteSheet, "noir");
+    else if (this.couleur == "normal")
+        this.animation = new createjs.Sprite(this.spriteSheet, "normal");
+    else
+        this.animation = new createjs.Sprite(this.spriteSheet, "malade");
+
+    this.animation.addEventListener("click", handleClick);
+    var self = this;
+
+    function handleClick(event) {
+
+        if (couleur == "noir")
+            perdre();
+        else if (couleur == "normal") {
+            self.detruire();
+            self.supprimer(self);
+        }
+        else {
+            self.detruire();
+            self.supprimer(self);
+        }
+    }
+
+    this.detruire = function(){
+        this.animation.removeAllEventListeners();
+    }
+
+
+
+
+}
